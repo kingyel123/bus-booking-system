618c1a45037d291837cfec7aff7d19b6667b4969
Chimi19 <chimirigdenwangdi@gmail.com>
2024-11-26 15:43:18 +0600

    BBS_02: Made bus model, service and controller. And created post api function

---
+++ src/main/java/com/java/busbookingsystem/bus/businfo/service/IBusService.java
@@ -0,0 +1,7 @@
+package com.java.busbookingsystem.bus.businfo.service;
+
+import com.java.busbookingsystem.bus.businfo.model.Bus;
+import com.java.busbookingsystem.bus.businfo.utils.IGenericCrudService;
+
+public interface IBusService extends IGenericCrudService<Bus> {
+}
