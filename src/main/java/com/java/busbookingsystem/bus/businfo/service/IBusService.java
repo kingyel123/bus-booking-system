75eb97365e3918b8c1bdea58606e5f37894096aa
Chimi19 <chimirigdenwangdi@gmail.com>
2024-11-27 00:29:35 +0600

    BMS_03: Created Rest of the crud operation, get,put,delete

--- src/main/java/com/java/busbookingsystem/bus/businfo/service/IBusService.java
+++ src/main/java/com/java/busbookingsystem/bus/businfo/service/IBusService.java
@@ -2,6 +2,7 @@ package com.java.busbookingsystem.bus.businfo.service;
 
 import com.java.busbookingsystem.bus.businfo.model.Bus;
 import com.java.busbookingsystem.bus.businfo.utils.IGenericCrudService;
+import lombok.NonNull;
 
 public interface IBusService extends IGenericCrudService<Bus> {
 }
