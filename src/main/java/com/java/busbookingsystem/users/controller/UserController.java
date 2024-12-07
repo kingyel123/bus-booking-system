91e8118aee098d024f02a2955ae7c3984d477423
rinchen1789 <rinchen1788@gmail.com>
2024-12-07 11:02:21 +0600

    BBS_10: Implemented delete API

--- src/main/java/com/java/busbookingsystem/users/controller/UserController.java
+++ src/main/java/com/java/busbookingsystem/users/controller/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
      *
      * @return The details of the authenticated user.
      */
+
     @GetMapping("/self")
     @PreAuthorize("hasAuthority('USER')")
     public ResponseEntity<RestResponse> fetchSelfInfo() {
@@ -39,6 +40,7 @@ public class UserController {
      * @param id The unique identifier of the instructor.
      * @return The instructor entity.
      */
+
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<RestResponse> findById(@PathVariable long id) {
@@ -47,11 +49,13 @@ public class UserController {
         return RestHelper.responseSuccess(listHashMap);
     }
 
+
     /**
      * Fetches all the instructor entities in the system.
      *
      * @return The list of instructor entities.
      */
+
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<RestResponse> findAll() {
@@ -59,7 +63,6 @@ public class UserController {
         listHashMap.put("user", userService.findAll());
         return RestHelper.responseSuccess(listHashMap);
     }
-
     /**
      * Signing up the new instructor.
      *
@@ -79,12 +82,7 @@ public class UserController {
      * @param user The updated instructor entity.
      * @return The message indicating the confirmation on updated instructor entity.
      */
-    @PatchMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('ADMIN','USER')")
-    public ResponseEntity<RestResponse> update(@PathVariable long id, @Validated @RequestBody User user) {
-        String message = userService.update(id, user);
-        return RestHelper.responseMessage(message);
-    }
+
 
     /**
      * Deletes the instructor by id.
@@ -92,10 +90,12 @@ public class UserController {
      * @param id The unique identifier of the entity.
      * @return The message indicating the confirmation on deleted instructor entity.
      */
+
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<RestResponse> delete(@PathVariable long id) {
         String message = userService.deleteById(id);
         return RestHelper.responseMessage(message);
-    }
+        }
+
 }
--- src/main/java/com/java/busbookingsystem/users/model/UserDTO.java
+++ src/main/java/com/java/busbookingsystem/users/model/UserDTO.java
@@ -13,4 +13,44 @@ public class UserDTO {
     private String address;
     private String email;
     private String roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
